98abdc74cdbb3dfc63771be41eafea1d5abc1d98
Adam Cunnington <adamcunnington.info@gmail.com>
2021-01-10 00:04:26 +0000

    Add Manifest to specify package data

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ with open("requirements.txt") as f:
 setuptools.setup(
     name="Bible",
     packages=setuptools.find_packages(),
+    include_package_data=True,
     install_requires=dependencies,
     extras_require={
         "dev": [
